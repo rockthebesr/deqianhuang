68ec44ad7d8c577939e6e22f8cd820fb27d8118c
rockluo <rockthebesr@gmail.com>
2020-10-03 14:35:23 -0400

    add lazy load

--- src/components/imageAlbum/imageAlbum.js
+++ src/components/imageAlbum/imageAlbum.js
@@ -24,6 +24,7 @@ export default function ImageAlbum(props) {
             <img
               className="d-block w-100 img-responsive"
               src={image}
+              loading="lazy"
               alt="info"
             />
           </Carousel.Item>
